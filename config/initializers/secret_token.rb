951199e1d259f29f1a5b581098187bb94ebf5722
ObaidMalik <obaid_malik@external.mckinsey.com>
2012-09-11 01:31:09 +0530

    Base Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ozfirst::Application.config.secret_token = 'd1e53549f7936e79696a48514dc56755565c0e0345dfe219d73b105d34baede4977829a067dcba7c5190d3789910d1f8a96869db0365479d83ebc0b337a63ad2'
